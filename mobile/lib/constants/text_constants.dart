563e7f9820fc4ccc92594aced06098b7aab6e57c
thaidmfinnick <thai.dm279@gmail.com>
2024-12-01 22:04:15 +0700

    feature(mobile): home screen

---
+++ mobile/lib/constants/text_constants.dart
@@ -0,0 +1,15 @@
+const adminStatistic = {
+  'scraper': {
+    'count_scraper': "Số lượng người tham gia",
+    'kg_co2e_reduced': "Tổng kg CO₂e giảm được",
+    'kg_collected': "Tổng kg rác thu gom được",
+    'expense_reduced': "Chi phí giảm được nhờ thu gom",
+  },
+
+  'household': {
+    'count_household': "Số lượng người tham gia",
+    'kg_co2e_plastic_reduced': "Lượng kg CO₂e giảm được từ giảm sử dụng nhựa",
+    'kg_co2e_recycle_reduced': "Lượng kg CO₂e giảm được từ tái chế",
+    'kg_recycle_collected': "Khối lượng rác tái chế thu gom được",
+  }
+};
