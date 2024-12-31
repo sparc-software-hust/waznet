9ff55cc2b607043a274893ac93c29c601c2edf85
dath7 <hoangdat24112002@gmail.com>
2024-12-31 22:20:24 +0700

    save token

--- mobile/lib/features/authentication/repository/authentication_repository.dart
+++ mobile/lib/features/authentication/repository/authentication_repository.dart
@@ -19,6 +19,9 @@ class AuthRepository {
     try {
       final Map res = await AuthenticationApi.login(phoneNumber, password);
       final bool isLoginSuccess = res["success"];
+      await AuthRepository.saveTokenDataIntoPrefs(res["data"]);
+      await UserRepository.saveUserDataIntoPrefs(res["data"]["user"]);
+      
       return isLoginSuccess;
     } catch (e) {
       return false;
