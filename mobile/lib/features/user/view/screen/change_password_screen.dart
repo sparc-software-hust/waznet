80b4984fc767219348efe7fe06aa43153bda8428
dath7 <hoangdat24112002@gmail.com>
2025-01-02 15:48:41 +0700

    format code

--- mobile/lib/features/user/view/screen/change_password_screen.dart
+++ mobile/lib/features/user/view/screen/change_password_screen.dart
@@ -36,58 +36,56 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
         context.watch<AuthenticationBloc>().state.user?.phoneNumber ?? "";
 
     return Scaffold(
-        backgroundColor: const Color(0xFFF4F4F5),
-        body: SafeArea(
-            top: false,
-            child: Column(children: [
-              HeaderWidget(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 25),
-                  child: Row(
-                    children: [
-                      InkWell(
-                        child: Icon(
-                          PhosphorIcons.regular.arrowLeft,
-                          size: 20,
-                          color: const Color(0xff29292A),
-                        ),
-                        onTap: () => Navigator.pop(context),
+      backgroundColor: const Color(0xFFF4F4F5),
+      body: SafeArea(
+        top: false,
+        child: Column(
+          children: [
+            HeaderWidget(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: Row(
+                  children: [
+                    InkWell(
+                      child: Icon(
+                        PhosphorIcons.regular.arrowLeft,
+                        size: 20,
+                        color: const Color(0xff29292A),
                       ),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      Text(
-                        'Thay đổi mật khẩu',
-                        style: colorConstants.fastStyle(
-                            16, FontWeight.w600, const Color(0xff29292A)),
-                      ),
-                    ],
-                  ),
+                      onTap: () => Navigator.pop(context),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      'Thay đổi mật khẩu',
+                      style: colorConstants.fastStyle(16, FontWeight.w600, const Color(0xff29292A)),
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 30),
-                  decoration: BoxDecoration(
-                      color: const Color(0xffFFFFFF),
-                      borderRadius: BorderRadius.circular(12)),
-                  padding: const EdgeInsets.all(24),
-                  child: BlocBuilder<ChangePasswordBloc, ChangePasswordState>(
-                      builder: (context, state) {
-                    if (state.status == ProgressStatus.notAuthenticated ||
-                        state.status == ProgressStatus.init) {
-                      return Column(
-                        children: [
-                          Text(
-                            "Nhập mật khẩu hiện tại",
-                            style: colorConstants.fastStyle(
-                                20, FontWeight.w700, const Color(0xff333334)),
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          BlocBuilder<ChangePasswordBloc, ChangePasswordState>(
-                              builder: (context, state) {
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 30),
+              decoration: BoxDecoration(
+                color: const Color(0xffFFFFFF),
+                borderRadius: BorderRadius.circular(12)
+              ),
+              padding: const EdgeInsets.all(24),
+              child: BlocBuilder<ChangePasswordBloc, ChangePasswordState>(
+                builder: (context, state) {
+                  if (state.status == ProgressStatus.notAuthenticated ||  state.status == ProgressStatus.init) {
+                    return Column(
+                      children: [
+                        Text(
+                          "Nhập mật khẩu hiện tại",
+                          style: colorConstants.fastStyle(20, FontWeight.w700, const Color(0xff333334)),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        BlocBuilder<ChangePasswordBloc, ChangePasswordState>(
+                          builder: (context, state) {
                             return CustomTextField(
                               label: "Mật khẩu",
                               placeholder: "Nhập mật khẩu của bạn",
@@ -102,75 +100,71 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                                 }
                                 return true;
                               },
-                              errorText: state.status ==
-                                      ProgressStatus.notAuthenticated
-                                  ? "Sai mật khẩu"
-                                  : "Mật khẩu phải có ít nhất 8 ký tự",
-                              isPassword: true,
-                              callback: (value, keyword) {
-                                setState(() {
-                                  oldPassword = value;
-                                });
-                                context
-                                    .read<ChangePasswordBloc>()
-                                    .add(OnTypePassword(password: value));
-                              },
-                            );
-                          }),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 12, bottom: 20),
-                            child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text("Quên mật khẩu",
-                                    style: colorConstants.fastStyle(
-                                        14,
-                                        FontWeight.w500,
-                                        const Color(0xff4CAF50)))),
-                          ),
-                          ElevatedButton(
-                            onPressed: () async {
+                            errorText: state.status ==
+                                    ProgressStatus.notAuthenticated
+                                ? "Sai mật khẩu"
+                                : "Mật khẩu phải có ít nhất 8 ký tự",
+                            isPassword: true,
+                            callback: (value, keyword) {
+                              setState(() {
+                                oldPassword = value;
+                              });
                               context
                                   .read<ChangePasswordBloc>()
-                                  .add(CheckPassWord(phoneNumber: phoneNumber));
+                                  .add(OnTypePassword(password: value));
                             },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFF4CAF50),
-                              minimumSize: const Size.fromHeight(50),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
+                          );
+                        }),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12, bottom: 20),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
                             child: Text(
-                              'Tiếp tục',
-                              style: colorConstants.fastStyle(
-                                  16, FontWeight.w700, const Color(0xffFFFFFF)),
+                              "Quên mật khẩu",
+                              style: colorConstants.fastStyle(14, FontWeight.w500, const Color(0xff4CAF50))
+                            )
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            context
+                                .read<ChangePasswordBloc>()
+                                .add(CheckPassWord(phoneNumber: phoneNumber));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF4CAF50),
+                            minimumSize: const Size.fromHeight(50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
                             ),
                           ),
-                        ],
-                      );
-                    }
+                          child: Text(
+                            'Tiếp tục',
+                            style: colorConstants.fastStyle(16, FontWeight.w700, const Color(0xffFFFFFF)),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
 
-                    return Column(
-                      children: [
-                        Text(
-                          "Nhập mật khẩu mới",
-                          style: colorConstants.fastStyle(
-                              20, FontWeight.w700, const Color(0xff333334)),
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        Text(
-                          "Mật khẩu mới của bạn phải khác với những mật khẩu đã sử dụng trước đây",
-                          style: colorConstants.fastStyle(
-                              14, FontWeight.w500, const Color(0xff666667)),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        BlocBuilder<ChangePasswordBloc, ChangePasswordState>(
-                          builder: (context, state) {
-
+                  return Column(
+                    children: [
+                      Text(
+                        "Nhập mật khẩu mới",
+                        style: colorConstants.fastStyle(20, FontWeight.w700, const Color(0xff333334)),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "Mật khẩu mới của bạn phải khác với những mật khẩu đã sử dụng trước đây",
+                        style: colorConstants.fastStyle(14, FontWeight.w500, const Color(0xff666667)),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      BlocBuilder<ChangePasswordBloc, ChangePasswordState>(
+                        builder: (context, state) {
                           bool valid =  state.passwordStatus == PasswordStatus.valid && state.status == ProgressStatus.authenticated;
 
                           return Column(
@@ -189,8 +183,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                                 keyword: "password",
                                 hasBorder: true,
                                 validator: (value) {
-                                  if (state.passwordStatus == PasswordStatus.sameAsOld ||
-                                      state.passwordStatus ==  PasswordStatus.notValid) {
+                                  if (state.passwordStatus == PasswordStatus.sameAsOld || state.passwordStatus ==  PasswordStatus.notValid) {
                                     return false;
                                   }
                                   return true;
@@ -225,9 +218,9 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                                   keyword: "password",
                                   hasBorder: true,
                                   validator: (value) {
-                                    if (state.passwordStatus ==  PasswordStatus.notMatch ||
-                                        state.passwordStatus == PasswordStatus.notValid ||
-                                        state.status == ProgressStatus.fail) {
+                                    if (state.passwordStatus ==  PasswordStatus.notMatch 
+                                        || state.passwordStatus == PasswordStatus.notValid
+                                        || state.status == ProgressStatus.fail) {
                                           
                                       return false;
                                     }
@@ -252,22 +245,22 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                               ElevatedButton(
                                 onPressed: valid
                                     ? () async {
-                                        context.read<ChangePasswordBloc>().add(
-                                            OnSubmitted(
-                                                newPassword: newPassword,
-                                                oldPassword: oldPassword,
-                                                callback: (value) {
-                                                  fToast.showToast(
-                                                    child: ToastContent(
-                                                      isSuccess: value, 
-                                                      title: value ? 'Cập nhật thành công' : 'Cập nhật thất bại. Vui lòng thử lại sau'
-                                                    ),
-                                                    gravity: ToastGravity.BOTTOM
-                                                  );
-                                                  Navigator.pop(context);
-                                                }
-                                              )
-                                            );                                        
+                                      context.read<ChangePasswordBloc>().add(
+                                        OnSubmitted(
+                                            newPassword: newPassword,
+                                            oldPassword: oldPassword,
+                                            callback: (value) {
+                                              fToast.showToast(
+                                                child: ToastContent(
+                                                  isSuccess: value, 
+                                                  title: value ? 'Cập nhật thành công' : 'Cập nhật thất bại. Vui lòng thử lại sau'
+                                                ),
+                                                gravity: ToastGravity.BOTTOM
+                                              );
+                                              Navigator.pop(context);
+                                            }
+                                          )
+                                        );                                        
                                     }
                                     : null,
                                 style: ElevatedButton.styleFrom(
@@ -281,20 +274,21 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                                 ),
                                 child: Text(
                                   'Lưu lại',
-                                  style: colorConstants.fastStyle(
-                                      16,
-                                      FontWeight.w700,
-                                      valid
-                                          ? const Color(0xffFFFFFF)
-                                          : const Color(0xffF4F4F5)),
+                                  style: colorConstants.fastStyle(16, FontWeight.w700, valid ? const Color(0xffFFFFFF) : const Color(0xffF4F4F5)),
                                 ),
                               ),
                             ],
                           );
-                        }),
-                      ],
-                    );
-                  }))
-            ])));
+                        }
+                      ),
+                    ],
+                  );
+                }
+              )
+            )
+          ]
+        )
+      )
+    );
   }
 }
